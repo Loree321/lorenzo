3d4f1759632a17ab60e6ead76b68f0bcfc9233bc
Loree321 <142606755+Loree321@users.noreply.github.com>
2023-09-04 10:40:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".multiple-text" , {
+    strings: ["FullStack Developer" , "English Coach"],
+    typeSpeed : 80,
+    backSpeed : 30,
+    backDelay : 1000,
+    loop : true,
+})
